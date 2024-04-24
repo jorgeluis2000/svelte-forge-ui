4fbf12755eb48113edb546f9f47f8fd32b9976e3
Jorge Guiza <jorgeluis.gg.2000@gmail.com>
2024-04-24 01:13:51 -0500

    ✨ Feat: New icons are added for use in components, and new functionalities are added for inputs. ✨

--- src/lib/constants/Styles.constants.ts
+++ src/lib/constants/Styles.constants.ts
@@ -1,4 +1,4 @@
-import type { RoundedSize } from "./../domains/types/Sizes.type";
+import type { RoundedSize, TextSize } from "./../domains/types/Sizes.type";
 
 export const ROUNDED_STYLE: { type: RoundedSize; class: string }[] = [
     { type: 'none', class: 'rounded-none' },
@@ -10,4 +10,20 @@ export const ROUNDED_STYLE: { type: RoundedSize; class: string }[] = [
     { type: '2xl', class: 'rounded-2xl' },
     { type: '3xl', class: 'rounded-3xl' },
     { type: 'full', class: 'rounded-full' }
+];
+
+export const TEXT_SIZE_STYLE: { type: TextSize; class: string }[] = [
+    { type: 'xs', class: 'text-xs' },
+    { type: 'sm', class: 'text-sm' },
+    { type: 'base', class: 'text-base' },
+    { type: 'lg', class: 'text-lg' },
+    { type: 'xl', class: 'text-xl' },
+    { type: '2xl', class: 'text-2xl' },
+    { type: '3xl', class: 'text-3xl' },
+    { type: '4xl', class: 'text-4xl' },
+    { type: '5xl', class: 'text-5xl' },
+    { type: '6xl', class: 'text-6xl' },
+    { type: '7xl', class: 'text-7xl' },
+    { type: '8xl', class: 'text-8xl' },
+    { type: '9xl', class: 'text-9xl' },
 ];
--- src/lib/functions/Styles.functions.ts
+++ src/lib/functions/Styles.functions.ts
@@ -1,5 +1,9 @@
-import type { RoundedSize } from "./../domains/types/Sizes.type";
+import type { RoundedSize, TextSize } from "./../domains/types/Sizes.type";
 
 export function getRoundedStyle(listStyleRounded: { type: RoundedSize; class: string }[], roundedSelected: RoundedSize) {
     return listStyleRounded.find((itemRounded) => itemRounded.type === roundedSelected);
+}
+
+export function getTextSizeStyle(listStyleTextSize: { type: TextSize; class: string }[], textSizeSelected: TextSize) {
+    return listStyleTextSize.find((itemTextSize) => itemTextSize.type === textSizeSelected);
 }
