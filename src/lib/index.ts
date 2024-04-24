e5e5938137927ed1cdf3eb28ef2597f41a34740f
Jorge Guiza <jorgeluis.gg.2000@gmail.com>
2024-04-23 22:04:11 -0500

    ✨ Feat: Making first components about inputs simples and floats.  ✨

---
+++ src/lib/index.ts
@@ -0,0 +1,4 @@
+import InputNumber from "./inputs/simples/InputNumber.svelte";
+import InputNumberFloat from "./inputs/floats/InputNumberFloat.svelte";
+
+export { InputNumber, InputNumberFloat }
---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import InputNumberFloat from '$lib/inputs/floats/InputNumberFloat.svelte';
+	import InputNumber from '$lib/inputs/simples/InputNumber.svelte';
+	let inputNumberValue = '';
+	let inputNumberFloatValue = '';
+</script>
+
+<section class="m-5 space-y-10">
+	<div class="flex w-full" id="inputs">
+		<h1 class="text-2xl font-bold">Inputs Section</h1>
+	</div>
+	<div class="flex w-full" id="inputs">
+		<h2 class="text-xl font-semibold">Inputs Section</h2>
+	</div>
+	<div>
+		<InputNumber
+			className=""
+			labelText="Input Number"
+			nameInput="input-number"
+			placeholder="placeholder"
+			bind:inputValue={inputNumberValue}
+		/>
+	</div>
+
+	<div>
+		<InputNumberFloat
+			className=""
+			nameInput="input-number-float"
+			labelText="Input Number Float"
+			bind:valueInput={inputNumberFloatValue}
+			maxlength={2}
+		/>
+	</div>
+</section>
