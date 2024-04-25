128ae79221fcd201ea936c23af2c648bbce81d23
Jorge Guiza <jorgeluis.gg.2000@gmail.com>
2024-04-24 19:57:41 -0500

    ♿️ Chore: We improved accessibility when exporting icons to the repository, in addition to allowing access to the types we are using. ♿️

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,5 +3,11 @@ import InputFloat from './components/inputs/floats/InputFloat.svelte';
 import InputSelectFloat from './components/inputs/floats/InputSelectFloat.svelte';
 import InputUnitSelectFloat from './components/inputs/floats/InputUnitSelectFloat.svelte';
 import InputNatural from './components/inputs/simples/InputNatural.svelte';
+import type { ItemSelect } from './domains/types/Select.type';
+import type { GroupItemSelect } from './domains/types/Select.type';
+import type { RoundedSize } from './domains/types/Sizes.type';
+import type { TextSize } from './domains/types/Sizes.type';
+import type { AutoCompleteInput } from './domains/types/AutoComplete.type';
 
+export type { ItemSelect, GroupItemSelect, RoundedSize, TextSize, AutoCompleteInput };
 export { InputBasic, InputFloat, InputSelectFloat, InputUnitSelectFloat, InputNatural };
