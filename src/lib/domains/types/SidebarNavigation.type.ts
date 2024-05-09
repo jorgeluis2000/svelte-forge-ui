b199ad1524e155cf21ad55c7580a1f35a114e365
Jorge Guiza <jorgeluis.gg.2000@gmail.com>
2024-05-09 10:34:53 -0500

    🏷️✏️ Chore: Type improvements for sidebars and tabs. ✏️🏷️

--- src/lib/domains/types/SidebarNavigation.type.ts
+++ src/lib/domains/types/SidebarNavigation.type.ts
@@ -1,26 +1,29 @@
 import type { ComponentType } from "svelte";
+import type { TypeItem } from "./TypeButton.type";
 
 export type ItemList = {
     opened: boolean;
-    isList: boolean;
-    icon: ComponentType;
+    isList?: boolean;
+    icon?: ComponentType;
     nameItem: string;
-    href: string;
+    href?: string;
     subItems: SubItems[];
 }
 
 export type SubItems = {
     href?: string;
+    type?: TypeItem
     text: string;
 }
 
 
 export type ItemTabList = {
     opened: boolean
-    isList: boolean
+    isList?: boolean
+    type?: TypeItem
     color: string
     nameItem: string
-    href: string
+    href?: string
     icon: ComponentType
     items: GroupItemSubMenuTabList[]
 }
@@ -32,7 +35,8 @@ export type GroupItemSubMenuTabList = {
 
 export type ItemSubMenuTabList = {
     color: string
+    type?: TypeItem
     text: string
-    href: string
+    href?: string
     icon: ComponentType
 }
