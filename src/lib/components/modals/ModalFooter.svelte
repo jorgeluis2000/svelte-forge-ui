96f3e0a48da0b48bb3fbec7d5e1c30c8702ef387
Jorge Guiza <jorgeluis.gg.2000@gmail.com>
2024-05-10 14:38:30 -0500

    ⚡️ Chore(Modals): 2 components are added that complement the structure of a Modal (It is not necessary to use them). ⚡️

---
+++ src/lib/components/modals/ModalFooter.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import ButtonAction from '$lib/components/buttons/ButtonAction.svelte';
+	import {
+		DEFAULT_COLOR_ERROR,
+		DEFAULT_COLOR_SUCCESS
+	} from '$lib/constants/DefaultStyles.constants';
+	import type { RoundedSize, TextSize } from '$lib/domains/types/Sizes.type';
+
+	export let showModal: boolean;
+	export let rounded: RoundedSize = 'none';
+	export let textSize: TextSize = 'base';
+	export let textCancel: string = '';
+	export let textAction: string = '';
+	export let className: string = '';
+
+	const dispatch = createEventDispatcher();
+</script>
+
+<section class={`container ${className}`}>
+	<ButtonAction
+		colorHex={DEFAULT_COLOR_ERROR}
+		{textSize}
+		{rounded}
+		on:click={() => {
+			showModal = false;
+			dispatch('cancel');
+		}}
+	>
+		{@html textCancel}
+	</ButtonAction>
+	<ButtonAction
+		isFilled
+		colorHex={DEFAULT_COLOR_SUCCESS}
+		{textSize}
+		{rounded}
+		on:click={() => dispatch('click')}
+	>
+		{@html textAction}
+	</ButtonAction>
+</section>
+
+<style lang="postcss">
+	.container {
+		@apply flex flex-col justify-center gap-3 items-center sm:flex-row;
+	}
+</style>
