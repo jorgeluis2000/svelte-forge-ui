4cf6773b5846fe7c0bc0b783332b6302573e4996
Jorge Guiza <jorgeluis.gg.2000@gmail.com>
2024-05-18 20:44:06 -0500

    🐛 Fix(Components): Fixed error when trying to do a global CSS import. 🐛

--- src/lib/components/toasts/Toast.svelte
+++ src/lib/components/toasts/Toast.svelte
@@ -14,7 +14,6 @@
 	import { DEFAULT_ALARM, DEFAULT_SIZE } from '$lib/constants/DefaultStyles.constants';
 	import type { TypeToast } from '$lib/domains/interfaces/Toast.interface';
 	import type { GeneralSize } from '$lib/domains/types/Sizes.type';
-	import '$lib/css/sizes.css';
 
 	const dispatch = createEventDispatcher();
 
@@ -125,5 +124,140 @@
 		background-color: var(--colorIconBg);
 		color: var(--colorIconText);
 		@apply flex justify-center items-center size-8 rounded-md;
+	}
+
+	.text-xs {
+		font-size: 0.75rem /* 12px */;
+		line-height: 1rem /* 16px */;
+	}
+
+	.text-sm {
+		font-size: 0.875rem /* 14px */;
+		line-height: 1.25rem /* 20px */;
+	}
+
+	.text-base {
+		font-size: 1rem /* 16px */;
+		line-height: 1.5rem /* 24px */;
+	}
+
+	.text-lg {
+		font-size: 1.125rem /* 18px */;
+		line-height: 1.75rem /* 28px */;
+	}
+
+	.text-xl {
+		font-size: 1.25rem /* 20px */;
+		line-height: 1.75rem /* 28px */;
+	}
+
+	.text-2xl {
+		font-size: 1.5rem /* 24px */;
+		line-height: 2rem /* 32px */;
+	}
+
+	.text-3xl {
+		font-size: 1.875rem /* 30px */;
+		line-height: 2.25rem /* 36px */;
+	}
+
+	.text-4xl {
+		font-size: 2.25rem /* 36px */;
+		line-height: 2.5rem /* 40px */;
+	}
+
+	.text-5xl {
+		font-size: 3rem /* 48px */;
+		line-height: 1;
+	}
+
+	.text-6xl {
+		font-size: 3.75rem /* 60px */;
+		line-height: 1;
+	}
+
+	.text-7xl {
+		font-size: 4.5rem /* 72px */;
+		line-height: 1;
+	}
+
+	.text-8xl {
+		font-size: 6rem /* 96px */;
+		line-height: 1;
+	}
+
+	.text-9xl {
+		font-size: 8rem /* 128px */;
+		line-height: 1;
+	}
+
+	/* Rounded */
+
+	.rounded-none {
+		border-radius: 0px;
+	}
+
+	.rounded-sm {
+		border-radius: 0.125rem /* 2px */;
+	}
+
+	.rounded {
+		border-radius: 0.25rem /* 4px */;
+	}
+
+	.rounded-md {
+		border-radius: 0.375rem /* 6px */;
+	}
+
+	.rounded-lg {
+		border-radius: 0.5rem /* 8px */;
+	}
+
+	.rounded-xl {
+		border-radius: 0.75rem /* 12px */;
+	}
+
+	.rounded-2xl {
+		border-radius: 1rem /* 16px */;
+	}
+
+	.rounded-3xl {
+		border-radius: 1.5rem /* 24px */;
+	}
+
+	.rounded-full {
+		border-radius: 9999px;
+	}
+
+	/* Size w and h */
+
+	.size-2\.5 {
+		width: 0.625rem /* 10px */;
+		height: 0.625rem /* 10px */;
+	}
+
+	.size-5 {
+		width: 1.25rem /* 20px */;
+		height: 1.25rem /* 20px */;
+	}
+
+	.size-6 {
+		width: 1.5rem /* 24px */;
+		height: 1.5rem /* 24px */;
+	}
+
+	.size-8 {
+		width: 2rem /* 32px */;
+		height: 2rem /* 32px */;
+	}
+
+	.size-14 {
+		width: 3.5rem /* 56px */;
+		height: 3.5rem /* 56px */;
+	}
+
+	.size-16 {
+		width: 4rem /* 64px */;
+		height: 4rem /* 64px */;
 	}
 </style>
