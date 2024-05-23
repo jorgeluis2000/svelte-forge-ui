aadebab3dc459e86a00564854ed623e74fe5c382
Jorge Guiza <jorgeluis.gg.2000@gmail.com>
2024-05-23 08:01:21 -0500

    💄 Chore: The changes made in the toasts are implemented. 💄

--- src/routes/toasts/+page.svelte
+++ src/routes/toasts/+page.svelte
@@ -2,10 +2,10 @@
 	import ButtonAction from '$lib/components/buttons/ButtonAction.svelte';
 	import InputUnitSelectFloat from '$lib/components/inputs/floats/InputUnitSelectFloat.svelte';
 	import ContainerToast from '$lib/components/toasts/ContainerToast.svelte';
-	import type { ItemSelect } from '$lib';
-	import type { TypeToast } from '$lib/domains/interfaces/Toast.interface';
 	import TimeSandIcon from '$lib/icons/TimeSandIcon.svelte';
 	import { addLoadingToast, addToast, updateToast, generateID } from '$lib/stores/ToastStore';
+	import type { ItemSelect } from '$lib';
+	import type { TypeToast } from '$lib/domains/interfaces/Toast.interface';
 
 	let selectUnitTag: ItemSelect | null = null;
 	const listAlarm: ItemSelect[] = [
@@ -13,11 +13,11 @@
 		{ name: 'I am a error message', text: 'Error', value: 'error' },
 		{ name: 'I am a info message', text: 'Info', value: 'info' },
 		{ name: 'I am a warning message', text: 'Warning', value: 'warning' },
-		{ name: 'I am a loading message', text: 'Loading', value: 'loading' }
+		{ name: 'I am a loading message', text: 'Loading', value: 'loading' },
+		{ name: 'I am a time message', text: 'Time', value: 'time' }
 	];
 
 	function eventNewToast() {
-		console.log(selectUnitTag);
 		addToast({
 			message: selectUnitTag?.name ?? 'I am a error message',
 			type: (selectUnitTag?.value as TypeToast) ?? 'error',
@@ -49,43 +49,44 @@
 	}
 </script>
 
-<InputUnitSelectFloat
-	labelText="Icons toast"
-	nameInput="input-float-icon-select"
-	listItemsShow={listAlarm}
-	rounded="md"
-	textSize="sm"
-	textSizeTags="sm"
-	defaultText=" - "
-	placeholderSearch="Search"
-	bind:valueInput={selectUnitTag}
-/>
-
 <ContainerToast sizeIcon="sm"></ContainerToast>
+<section class="mx-2 md:mx-5 space-y-5">
+	<InputUnitSelectFloat
+		labelText="Icons toast"
+		nameInput="input-float-icon-select"
+		listItemsShow={listAlarm}
+		rounded="md"
+		textSize="sm"
+		textSizeTags="sm"
+		defaultText=" - "
+		placeholderSearch="Search"
+		bind:valueInput={selectUnitTag}
+	/>
 
-<div class="flex w-full" id="textareas">
-	<h2 class="text-xl font-semibold">Toasts Section</h2>
-</div>
+	<div class="flex w-full" id="textareas">
+		<h2 class="text-xl font-semibold">Toasts Section</h2>
+	</div>
 
-<section class="flex justify-center items-end gap-3">
-	<ButtonAction rounded="md" textSize="sm" thereIsIcon on:click={eventNewToast}>
-		<div slot="icon">
-			<TimeSandIcon />
-		</div>
-		Add a new Toast
-	</ButtonAction>
+	<section class="flex justify-center items-end gap-3">
+		<ButtonAction rounded="md" textSize="sm" thereIsIcon on:click={eventNewToast}>
+			<div slot="icon">
+				<TimeSandIcon />
+			</div>
+			Add a new Toast
+		</ButtonAction>
 
-	<ButtonAction rounded="md" textSize="sm" thereIsIcon on:click={eventNewToastLoading}>
-		<div slot="icon">
-			<TimeSandIcon />
-		</div>
-		Add a new Toast Loading
-	</ButtonAction>
+		<ButtonAction rounded="md" textSize="sm" thereIsIcon on:click={eventNewToastLoading}>
+			<div slot="icon">
+				<TimeSandIcon />
+			</div>
+			Add a new Toast Loading
+		</ButtonAction>
 
-	<ButtonAction rounded="md" textSize="sm" thereIsIcon on:click={eventNewToastCustom}>
-		<div slot="icon">
-			<TimeSandIcon />
-		</div>
-		Add a new Toast Custom
-	</ButtonAction>
+		<ButtonAction rounded="md" textSize="sm" thereIsIcon on:click={eventNewToastCustom}>
+			<div slot="icon">
+				<TimeSandIcon />
+			</div>
+			Add a new Toast Custom
+		</ButtonAction>
+	</section>
 </section>
